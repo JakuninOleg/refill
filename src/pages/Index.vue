d0024dea9161a589ea511ee5d66ef9b6861faeef
olegjakunin <oleg.kemz@gmail.com>
2019-11-26 18:35:13 +0300

    second build

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <Modal v-if="modalOpen" @toggleModal="toggleModal"/>
+    <Modal v-if="modalOpen" @toggleModal="toggleModal" />
 
     <Header>
-      <Navigation @toggleModal="toggleModal"/>
+      <Navigation @toggleModal="toggleModal" />
     </Header>
 
     <Description />
@@ -15,14 +15,8 @@
     <FormQuestions />
 
     <section class="map">
-      <yandex-map
-        :settings="yandexSettings"
-        :coords="[55.714404, 37.719136]"
-        style="width: 100%; height: 60rem; position: absolute;"
-        :controls="[]"
-      >
-        <ymap-marker markerId="1" :coords="[55.714404, 37.719136]" />
-      </yandex-map>
+      <div id="map" class="map"></div>
+   
       <div class="map__header">Адреса офисов в Москве</div>
     </section>
 
@@ -31,7 +25,6 @@
 </template>
 
 <script>
-import { yandexMap, ymapMarker } from "vue-yandex-maps";
 import Navigation from "@/components/Navigation.vue";
 import Header from "@/components/Header.vue";
 import Conditions from "@/components/Conditions.vue";
@@ -39,7 +32,7 @@ import Description from "@/components/Description.vue";
 import FormApplication from "@/components/FormApplication.vue";
 import FormQuestions from "@/components/FormQuestions.vue";
 import Footer from "@/components/Footer.vue";
-import Modal from '@/components/Modal.vue';
+import Modal from "@/components/Modal.vue";
 
 export default {
   components: {
@@ -49,24 +42,69 @@ export default {
     FormApplication,
     FormQuestions,
     Conditions,
-    yandexMap,
-    ymapMarker,
     Footer,
     Modal
   },
   data: () => {
     return {
-      yandexSettings: {
-        api:
-          "3A6d943a65558c35905c86a4cf36cb2c62c1897927b66ad5a15ada713aad7c1ec6",
-        lang: "ru_RU"
-      },
       modalOpen: false
+    };
+  },
+  mounted() {
+    this.load_yamap_obj(
+      "https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3A6d943a65558c35905c86a4cf36cb2c62c1897927b66ad5a15ada713aad7c1ec6"
+    ).then(() => {
+      window.ymaps.ready(this.init_map);
+    });
+    if (this.defaultService) {
+      this.selected_service = services[this.defaultService];
+    }
+  },
+  watch: {
+    myMap(value) {
+      if (value) {
+        this.reload_map(this);
+      }
     }
   },
   methods: {
+    load_yamap_obj(source, beforeEl, async = true, defer = true) {
+      return new Promise((resolve, reject) => {
+        let script = document.createElement("script");
+        const prior = beforeEl || document.getElementsByTagName("script")[0];
+        script.async = async;
+        script.defer = defer;
+        function onloadHander(_, isAbort) {
+          if (
+            isAbort ||
+            !script.readyState ||
+            /loaded|complete/.test(script.readyState)
+          ) {
+            script.onload = null;
+            script.onreadystatechange = null;
+            script = undefined;
+            if (isAbort) {
+              reject();
+            } else {
+              resolve();
+            }
+          }
+        }
+        script.onload = onloadHander;
+        script.onreadystatechange = onloadHander;
+        script.src = source;
+        prior.parentNode.insertBefore(script, prior);
+      });
+    },
+    init_map() {
+      this.myMap = new ymaps.Map("map", {
+        center: [55.714404, 37.719136], // Москва.
+        zoom: 17,
+        controls: []
+      });
+    },
     toggleModal() {
-      this.modalOpen = !this.modalOpen
+      this.modalOpen = !this.modalOpen;
     }
   }
 };
